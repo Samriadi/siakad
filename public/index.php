49f557ff481605e8c6f4729e762e4e2a16a104e4
samriadi <basosamriadi01@gmail.com>
2024-07-31 15:07:17 +0800

    require global name table

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ date_default_timezone_set('Asia/Makassar');
 
 require_once __DIR__ . '/../app/Core/Router.php';   
 require_once __DIR__ . '/../app/Core/Database.php';
+require_once __DIR__ . '/../app/Core/Table.php';
 // require_once __DIR__ . '/../app/Helpers/Function.php';
 // require_once __DIR__ . '/../app/Core/Table.php';
 
