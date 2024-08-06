8d7a5b0aa94410577c842a4af4b08a6d7901523b
baharuddin <baharuddindbd7@gmail.com>
2024-08-06 10:06:47 +0800

    add swal information

--- app/Views/others/page_mahasiswa.php
+++ app/Views/others/page_mahasiswa.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>SIAKAD - Mahasiswa</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>SIAKAD - Mahasiswa</title>
     <?php include '../app/Views/others/layouts/header.php'; ?>
 
-    <body>
+<body>
     <div id="app">
         <div class="main-wrapper main-wrapper-1">
 
@@ -16,108 +17,108 @@
             <!-- sidbar -->
             <?php include '../app/Views/others/layouts/sidebar.php'; ?>
 
-        <!-- Main Content -->
-        <div class="main-content">
-            <section class="section">
-            <div class="section-header">
-                <h1>Data</h1>
-                <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Data</a></div>
-                <div class="breadcrumb-item">Data Mahasiswa</div>
-                </div>
-            </div>
-
-            <div class="section-body">
-                <div class="row">
-                <div class="col-12 col-md-12 col-lg-12">
-                    <div class="card">
-                    <div class="card-header">
-                        <h4>Data Mahasiswa</h4>
-                        <div class="card-header-action">
-                            <button class="btn btn-primary" id="confirmButton">Import Data</button>
-
-
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section">
+                    <div class="section-header">
+                        <h1>Data</h1>
+                        <div class="section-header-breadcrumb">
+                            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                            <div class="breadcrumb-item"><a href="#">Data</a></div>
+                            <div class="breadcrumb-item">Data Mahasiswa</div>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <table class="table table-hover">
-                        <thead>
-                            <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Nama Lengkap</th>
-                            <th scope="col">Nim</th>
-                            <th scope="col">Wa Number</th>
-                            <th scope="col">Alamat</th>
-                            <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($this->dataMahasiswa as $key => $value) : ?>
-                            <tr>
-                                <th scope="row"><?= ++$key ?></th>
-                                <td><?= $value->NamaLengkap ?></td>
-                                <td><?= $value->Nim ?></td>
-                                <td><?= $value->WANumber ?></td>
-                                <td><?= $value->alamat ?></td>
-                                <td>
-                                    <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#editModal" data-id="<?= $value->ID ?>"><i class="fas fa-pencil-alt"></i></a>
-                                    <a class="btn btn-danger btn-action"><i class="fas fa-trash"></i></a>
-                                </td>   
-                            </tr>
-                        <?php endforeach ?>
-                        </tbody>
-                        </table>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </section>
-        </div>
-        
-      </div>
 
-      <!-- Modal Structure -->
-        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editModalLabel">Edit Data Mahasiswa</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="card-body">
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                    <label for="NamaLengkap">Nama Lengkap</label>
-                    <input type="text" class="form-control" id="NamaLengkap" placeholder="Nama Lengkap">
+                    <div class="section-body">
+                        <div class="row">
+                            <div class="col-12 col-md-12 col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Data Mahasiswa</h4>
+                                        <div class="card-header-action">
+                                            <button class="btn btn-primary" id="confirmButton">Import Data</button>
+
+
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">No</th>
+                                                    <th scope="col">Nama Lengkap</th>
+                                                    <th scope="col">Nim</th>
+                                                    <th scope="col">Wa Number</th>
+                                                    <th scope="col">Alamat</th>
+                                                    <th scope="col">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach ($this->dataMahasiswa as $key => $value) : ?>
+                                                    <tr>
+                                                        <th scope="row"><?= ++$key ?></th>
+                                                        <td><?= $value->NamaLengkap ?></td>
+                                                        <td><?= $value->Nim ?></td>
+                                                        <td><?= $value->WANumber ?></td>
+                                                        <td><?= $value->alamat ?></td>
+                                                        <td>
+                                                            <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#editModal" data-id="<?= $value->ID ?>"><i class="fas fa-pencil-alt"></i></a>
+                                                            <a class="btn btn-danger btn-action"><i class="fas fa-trash"></i></a>
+                                                        </td>
+                                                    </tr>
+                                                <?php endforeach ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                    <label for="nim">NIM</label>
-                    <input type="text" class="form-control" id="nim" placeholder="Nim">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="alamat">Alamat</label>
-                    <input type="text" class="form-control" id="alamat" placeholder="Alamat">
-                </div>
-                </div>
+                </section>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="submit">Submit</button>
-            </div>
-            </div>
+
         </div>
+
+        <!-- Modal Structure -->
+        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Edit Data Mahasiswa</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card-body">
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="NamaLengkap">Nama Lengkap</label>
+                                    <input type="text" class="form-control" id="NamaLengkap" placeholder="Nama Lengkap">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="nim">NIM</label>
+                                    <input type="text" class="form-control" id="nim" placeholder="Nim">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <input type="text" class="form-control" id="alamat" placeholder="Alamat">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="submit">Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Footer -->
         <?php include '../app/Views/others/layouts/footer.php'; ?>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        
+
         <script>
             $(document).ready(function() {
                 checkProgressClassOnLoad('confirmButton');
@@ -189,9 +190,11 @@
 
                     // Mengirim ID ke server melalui AJAX
                     $.ajax({
-                        url: '/siakad/mahasiswa/fetch', 
+                        url: '/siakad/mahasiswa/fetch',
                         type: 'POST',
-                        data: { id: id },
+                        data: {
+                            id: id
+                        },
                         dataType: 'json',
                         success: function(response) {
                             if (response.success) {
@@ -208,23 +211,34 @@
 
                     $('#submit').on('click', function() {
 
-                        var arrayData = [{ 
-                            id: id, 
+                        var arrayData = [{
+                            id: id,
                             alamat: $('#alamat').val()
                         }];
-                        console.log("🚀 ~ $ ~ arrayData:", arrayData)
+                        // console.log("🚀 ~ $ ~ arrayData:", arrayData)
 
                         $.ajax({
                             url: '/siakad/mahasiswa/update',
                             type: 'POST',
-                            contentType: 'application/json', 
+                            contentType: 'application/json',
                             data: JSON.stringify(arrayData),
                             dataType: 'json',
                             success: function(response) {
                                 if (response.success) {
-                                    console.log("🚀 ~ $ ~ response:", response)
+                                    Swal.fire({
+                                        text: 'Your data has been updated.',
+                                        icon: 'success',
+                                        showConfirmButton: false, // Menyembunyikan tombol konfirmasi
+                                        willClose: () => {
+                                            window.location.reload(); // Reload halaman setelah modal ditutup
+                                        }
+                                    });
                                 } else {
-                                    console.log("🚀 ~ $ ~ response:", response)
+                                    Swal.fire({
+                                        text: 'An error occurred during updated.',
+                                        icon: 'error',
+                                        confirmButtonText: 'OK'
+                                    });
 
                                 }
                             }
@@ -236,6 +250,7 @@
 
             });
         </script>
-      
-    </body>
+
+</body>
+
 </html>
