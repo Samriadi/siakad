e98d8462d8675dd4e034eaf25b16a807ada6e019
Samriadi <basosamriadi01@gmail.com>
2024-08-10 09:54:01 +0800

    update sidebar

--- app/Views/others/layouts/sidebar.php
+++ app/Views/others/layouts/sidebar.php
@@ -1,41 +1,51 @@
-   <div class="main-sidebar sidebar-style-2">
-     <aside id="sidebar-wrapper">
-       <div class="sidebar-brand">
-         <a href="index.html">SIAKAD</a>
-       </div>
-       <div class="sidebar-brand sidebar-brand-sm">
-         <!-- <a href="index.html">St</a> -->
-       </div>
-       <ul class="sidebar-menu">
-         <li class="menu-header">Dashboard</li>
-         <li class="dropdown">
-           <a href="" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-           <ul class="dropdown-menu">
-             <li class="nav-item"><a class="nav-link" href="/siakad/">General</a></li>
-           </ul>
-         </li>
-         <li class="menu-header">SIAKAD</li>
-         <li class="dropdown">
-           <a href="" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data</span></a>
-           <ul class="dropdown-menu">
-             <li><a class="nav-link" href="/siakad/mahasiswa">Mahasiswa</a></li>
-             <li><a class="nav-link" href="/siakad/ortu">Orang Tua</a></li>
-             <li><a class="nav-link" href="/siakad/dosen">Dosen</a></li>
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">SIAKAD</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Dashboard</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        <ul class="dropdown-menu">
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/">General</a></li>
+        </ul>
+      </li>
+      <li class="menu-header">SIAKAD</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Master</span></a>
+        <ul class="dropdown-menu">
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/mahasiswa">Mahasiswa</a></li>
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/ortu">Orang Tua</a></li>
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/dosen">Dosen</a></li>
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/staff">Staff</a></li>
+        </ul>
+      </li>
 
-           </ul>
-         </li>
+      <li class="menu-header">Pages</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
+        <ul class="dropdown-menu">
+          <li class="nav-item"><a href="/admin/siakad/login">Login</a></li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
+</div>
 
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const currentPath = window.location.pathname;
+    const menuItems = document.querySelectorAll('.sidebar-menu .nav-link');
 
-         <li class="menu-header">Pages</li>
-         <li class="dropdown">
-           <a href="" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-           <ul class="dropdown-menu">
-             <li><a href="/siakad/login">Login</a></li>
-           </ul>
-         </li>
-
-
-       </ul>
-
-     </aside>
-   </div>
+    menuItems.forEach(item => {
+      if (item.getAttribute('href') === currentPath) {
+        item.closest('.nav-item').classList.add('active');
+        item.closest('.dropdown').classList.add('active');
+        item.classList.add('active');
+      }
+    });
+  });
+</script>
