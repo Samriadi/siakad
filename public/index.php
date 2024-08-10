d4fd75972c3840c5c1077132c4819e189f1c86c8
Samriadi <basosamriadi01@gmail.com>
2024-08-11 07:13:36 +0800

    update index

--- public/index.php
+++ public/index.php
@@ -12,10 +12,14 @@ require_once __DIR__ . '/../app/Controllers/AuthController.php';
 require_once __DIR__ . '/../app/Controllers/MahasiswaController.php';
 require_once __DIR__ . '/../app/Controllers/DosenController.php';
 require_once __DIR__ . '/../app/Controllers/StaffController.php';
+require_once __DIR__ . '/../app/Controllers/MatkulController.php';
+require_once __DIR__ . '/../app/Controllers/PerkuliahanController.php';
 
 require_once __DIR__ . '/../app/Models/MahasiswaModel.php';
 require_once __DIR__ . '/../app/Models/DosenModel.php';
 require_once __DIR__ . '/../app/Models/StaffModel.php';
+require_once __DIR__ . '/../app/Models/MatkulModel.php';
+require_once __DIR__ . '/../app/Models/PerkuliahanModel.php';
 
 
 
@@ -44,6 +48,19 @@ $router->add('/staff/fetch', 'StaffController', 'fetchData');
 $router->add('/staff/add', 'StaffController', 'addData');
 $router->add('/staff/update', 'StaffController', 'updateData');
 $router->add('/staff/delete', 'StaffController', 'deleteData');
+
+$router->add('/matkul', 'MatkulController', 'index');
+$router->add('/matkul/fetch', 'MatkulController', 'fetchData');
+$router->add('/matkul/add', 'MatkulController', 'addData');
+$router->add('/matkul/update', 'MatkulController', 'updateData');
+$router->add('/matkul/delete', 'MatkulController', 'deleteData');
+
+$router->add('/perkuliahan', 'PerkuliahanController', 'index');
+$router->add('/perkuliahan/fetch', 'PerkuliahanController', 'fetchData');
+$router->add('/perkuliahan/add', 'PerkuliahanController', 'addData');
+$router->add('/perkuliahan/update', 'PerkuliahanController', 'updateData');
+$router->add('/perkuliahan/delete', 'PerkuliahanController', 'deleteData');
+$router->add('/perkuliahan/include', 'PerkuliahanController', 'includeData');
 
 
 
