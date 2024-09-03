636497226c67e15a0f7c304f2d4f08e0dee2210e
Samriadi <basosamriadi01@gmail.com>
2024-09-03 16:47:15 +0800

    update mahasiswa controller

--- app/Controllers/MahasiswaController.php
+++ app/Controllers/MahasiswaController.php
@@ -154,4 +154,54 @@ class MahasiswaController
 
         echo json_encode(['success' => $success]);
     }
+
+    public function importCSV()
+    {
+        if (isset($_POST['importCSV'])) {
+            if ($_FILES['file']['name']) {
+                $filename = explode(".", $_FILES['file']['name']);
+                if (end($filename) == "csv") {
+                    $handle = fopen($_FILES['file']['tmp_name'], "r");
+                    if ($handle !== FALSE) {
+                        try {
+                            // Skip the first line if CSV has headers
+                            fgetcsv($handle);
+
+                            while ($data = fgetcsv($handle)) {
+                                if ($data !== FALSE) {
+                                    // Data mapping, sesuai dengan urutan kolom di CSV
+                                    $NamaLengkap = $data[0];
+                                    $Nim = $data[1];
+                                    $WANumber = $data[2];
+                                    $alamat = $data[3];
+
+                                    // Simpan data ke dalam database menggunakan model
+                                    $this->MahasiswaModel->saveMahasiswa($NamaLengkap, $Nim, $WANumber, $alamat);
+                                } else {
+                                    // Log the error if a line can't be parsed
+                                    error_log("Error parsing CSV line: " . print_r($data, true));
+                                }
+                            }
+                            fclose($handle);
+                        } catch (Exception $e) {
+                            // Log any exceptions that occur during processing
+                            error_log("Exception caught during CSV import: " . $e->getMessage());
+                        }
+                    } else {
+                        // Log error if file can't be opened
+                        error_log("Error opening CSV file: " . $_FILES['file']['tmp_name']);
+                    }
+                } else {
+                    // Log error if file is not a CSV
+                    error_log("Uploaded file is not a CSV: " . $_FILES['file']['name']);
+                }
+            } else {
+                // Log error if no file was uploaded
+                error_log("No file uploaded for CSV import.");
+            }
+        } else {
+            // Log error if importCSV was not set
+            error_log("importCSV was not set in POST request.");
+        }
+    }
 }
