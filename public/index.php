aa706b1a6ed8681ca3633aadd90c377dad5bc7d0
Samriadi <basosamriadi01@gmail.com>
2024-09-05 15:56:07 +0800

    update index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 date_default_timezone_set('Asia/Makassar');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 require_once __DIR__ . '/../app/Core/Router.php';
 require_once __DIR__ . '/../app/Core/Database.php';
