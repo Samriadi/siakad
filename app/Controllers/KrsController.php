446ed3c24afe13b18bde704aeb5c2da251de5b33
Samriadi <basosamriadi01@gmail.com>
2024-09-20 12:01:00 +0800

    update krs controller

--- app/Controllers/KrsController.php
+++ app/Controllers/KrsController.php
@@ -3,10 +3,21 @@
 class KrsController
 {
 
-
-  public function __construct() {}
+  private $KrsModel;
+  private $matkul1;
+  private $matkul2;
+  public function __construct()
+  {
+    $this->KrsModel = new KrsModel();
+    $this->matkul1 = $this->KrsModel->getMatakuliah(1);
+    $this->matkul2 = $this->KrsModel->getMatakuliah(2);
+  }
   public function krs()
   {
+
+    $dataMatkul1 = $this->matkul1;
+    $dataMatkul2 = $this->matkul2;
+
     include __DIR__ . '/../Views/others/page_krs.php';
   }
 }
