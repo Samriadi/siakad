ecd61279e9871005cb60b1cda73b4909a2726a58
Samriadi <basosamriadi01@gmail.com>
2024-09-24 16:35:48 +0800

    add ad daya krs controller

--- app/Controllers/KrsController.php
+++ app/Controllers/KrsController.php
@@ -32,4 +32,44 @@ class KrsController
 
     include __DIR__ . '/../Views/others/page_krs.php';
   }
+
+    public function addData()
+  {
+    // Mengambil input JSON
+      $data = json_decode(file_get_contents('php://input'), true);
+      error_log(print_r($data, true));
+
+      // Validasi data
+      $academic_year = $data[0]['academic_year'] ?? null;
+      $semester = $data[0]['semester'] ?? null;
+      $student_id = $data[0]['student_id'] ?? null;
+      $total_credits = $data[0]['total_credits'] ?? null;
+      $selected_course_ids = $data[0]['selected_course_ids'] ?? null;
+      error_log(print_r($selected_course_ids, true));
+
+      $submission_date = date('Y-m-d');
+
+      if ($data === null) {
+        $response = [
+          'success' => false,
+          'message' => 'Invalid JSON input'
+        ];
+      } else {
+        $request = $this->KrsModel->addData($student_id, $semester, $academic_year, $total_credits, $submission_date);
+
+        if($request === true){
+          $this->KrsModel->addDataKrsCourses($student_id, $selected_course_ids);
+        }
+
+  
+        $response = [
+          'success' => $request,
+          'message' => $request ? 'Data successfully added' : 'Add data failed',
+          'data' => $data,
+        ];
+      }
+      header('Content-Type: application/json');
+      echo json_encode($response);
+  }
+        
 }
