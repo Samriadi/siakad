87957af731aebb506dc12d4a9d2bf1438fb706b6
Samriadi <basosamriadi01@gmail.com>
2024-10-04 13:48:55 +0800

    add logout siakad

--- app/Views/others/layouts/topbar.php
+++ app/Views/others/layouts/topbar.php
@@ -11,7 +11,7 @@
         
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-            <div class="d-sm-none d-lg-inline-block">Admin</div></a>
+            <div class="d-sm-none d-lg-inline-block"><?= $_SESSION['user_loged']?></div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <a href="features-profile.html" class="dropdown-item has-icon">
                 <i class="far fa-user"></i> Profile
@@ -20,10 +20,59 @@
                 <i class="fas fa-cog"></i> Settings
               </a>
               <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item has-icon text-danger">
+              <a id="logoutButton"  href="javascript:void(0);" class="dropdown-item has-icon text-danger">
                 <i class="fas fa-sign-out-alt"></i> Logout
               </a>
             </div>
           </li>
         </ul>
       </nav>
+      
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+document.getElementById('logoutButton').addEventListener('click', function() {
+           Swal.fire({
+               title: 'Are you sure?',
+               text: "You won't be able to revert this!",
+               icon: 'warning',
+               showCancelButton: true,
+               confirmButtonColor: '#3085d6',
+               cancelButtonColor: '#d33',
+               confirmButtonText: 'Yes, logout!'
+           }).then((result) => {
+               if (result.isConfirmed) {
+                   $.ajax({
+                       url: '/admin/logout',
+                       type: 'POST',
+                       success: function(response) {
+                           if (response.status === 'success') {
+                               Swal.fire({
+                                   title: 'Success!',
+                                   text: 'You have been logged out.',
+                                   icon: 'success',
+                                   timer: 1000,
+                                   showConfirmButton: false
+                               }).then((result) => {
+                                   window.location.href = '/admin';
+                               });
+                           } else {
+                               Swal.fire(
+                                   'Error!',
+                                   'There was a problem logging you out.',
+                                   'error'
+                               );
+                           }
+                       },
+                       error: function() {
+                           Swal.fire(
+                               'Error!',
+                               'There was a problem logging you out.',
+                               'error'
+                           );
+                       }
+                   });
+               }
+           });
+          });
+</script>
