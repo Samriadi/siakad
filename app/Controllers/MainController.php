6fe711d77b5b751f81ce8f6c7d6d3c2c95b569f0
Samriadi <basosamriadi01@gmail.com>
2024-10-08 15:43:04 +0800

    add auth

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -2,8 +2,16 @@
 
 class MainController{
     public function index(){
+        
+        if (!isset($_SESSION['user_loged'])) {
+            header("Location: /admin/login");
+            exit();
+        }
+    
+        // Jika sesi ada, tampilkan halaman dashboard
         include __DIR__ . '/../Views/others/page_dashboard.php';
     }
+    
     public function selectDash()
     {
         include __DIR__ . '/../Views/others/page_selectDash.php';
