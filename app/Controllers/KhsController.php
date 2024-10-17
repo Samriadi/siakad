6106adaa7e025494b37b2ac7b0fcb8d3e9336df0
Samriadi <basosamriadi01@gmail.com>
2024-10-17 13:34:00 +0800

    add check login

--- app/Controllers/KhsController.php
+++ app/Controllers/KhsController.php
@@ -2,6 +2,16 @@
 
 class KhsController
 {
+  public function __construct()
+  {
+    $this->checkLogin();
+  }
+  public function checkLogin() {
+    if (!isset($_SESSION['user_loged'])) {
+        header("Location: /admin/login");
+        exit();
+    }
+  }
   public function khs()
   {
     include __DIR__ . '/../Views/others/page_khs.php';
