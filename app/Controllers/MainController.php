68fc752e25ff89cff5657845d6749b6b12dfae18
Samriadi <basosamriadi01@gmail.com>
2024-10-18 16:10:08 +0800

    update controller

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -5,6 +5,8 @@ class MainController{
     public function __construct()
     {
       $this->checkLogin();
+      $this->MahasiswaModel = new MahasiswaModel();
+
     }
 
     public function checkLogin() {
@@ -29,5 +31,40 @@ class MainController{
     {
         include __DIR__ . '/../Views/others/page_selectDash.php';
     }
+
+     //pmb
+    public function indexRegist()
+    {
+        include __DIR__ . '/../Views/others/page_regist.php';
+    }
+
+    public function addRegist() {
+        $nik = $_POST['nik'] ?? ''; 
+        
+        // Mengecek apakah NIK sudah terdaftar
+        $checkNik = $this->MahasiswaModel->getDataRegistUsingNik($nik);
+        
+        if (!empty($checkNik)) {
+            $response = [
+                'status' => 'exist',
+                'message' => 'Anda sudah terdaftar sebelumnya. Silahkan login menggunakan NIK dan password Anda sebelumnya.'
+            ];
+        }
+        else{
+            // $addNik = $this->MahasiswaModel->addDataRegistUsingNik($nik);
+
+            $response =  ['status' => 'null', 'data' => $nik];
+        }
+        
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+
+    public function insertRegist()
+    {
+        include __DIR__ . '/../Views/others/page_insertRegist.php';
+    }
+    
+    
 
 }
