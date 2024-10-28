9a2efc1fd1a00644a765e4cc4de2f27afdd6d9f7
Samriadi <basosamriadi01@gmail.com>
2024-10-28 11:08:19 +0800

    update page mahasiswa

--- app/Views/others/page_mahasiswa.php
+++ app/Views/others/page_mahasiswa.php
@@ -19,64 +19,109 @@
 
         <!-- Main Content -->
         <div class="main-content">
-            <section class="section">
-                <div class="section-header">
-                    <h1>Data</h1>
-                    <div class="section-header-breadcrumb">
-                        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                        <div class="breadcrumb-item"><a href="#">Data</a></div>
-                        <div class="breadcrumb-item">Data Mahasiswa</div>
-                    </div>
-                </div>
+        <section class="section">
+        <div class="section-header">
+            <h1>Data Mahasiswa</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Data</a></div>
+                <div class="breadcrumb-item">Data Mahasiswa</div>
+            </div>
+        </div>
 
-                <div class="section-body">
-                    <div class="row">
-                        <div class="col-12 col-md-12 col-lg-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4>Data Mahasiswa</h4>
-                                    <div class="card-header-action">
-                                        <button class="btn btn-primary" data-toggle="modal" data-target="#importCSVModal">Import CSV</button>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                        <button type="button" class="btn btn-primary mr-2" data-toggle="modal" data-target="#multiFormModal">
+                           Add Data
+                        </button>
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#importCSVModal">Import CSV</button>
+                        </div>
+                        <div class="card-body">
+                            <ul class="nav nav-pills" id="myTab3" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab3" data-toggle="tab" href="#home3" role="tab" aria-controls="home" aria-selected="true">Mahasiswa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab3" data-toggle="tab" href="#profile3" role="tab" aria-controls="profile" aria-selected="false">Orang Tua</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="myTabContent2">
+                                <div class="tab-pane fade show active" id="home3" role="tabpanel" aria-labelledby="home-tab3">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="table-responsive" style="max-height: 500px; overflow-y: auto;">
+                                                <table class="table table-hover">
+                                                    <thead style="position: sticky; top: 0; background-color: #fff; z-index: 1;">
+                                                        <tr>
+                                                            <th scope="col">No</th>
+                                                            <th scope="col">Nama Lengkap</th>
+                                                            <th scope="col">Nim</th>
+                                                            <th scope="col">Wa Number</th>
+                                                            <th scope="col">Alamat</th>
+                                                            <th scope="col">Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php foreach ($data as $key => $value) : ?>
+                                                            <tr>
+                                                                <th scope="row"><?= ++$key ?></th>
+                                                                <td><?= $value->NamaLengkap ?></td>
+                                                                <td><?= $value->Nim ?></td>
+                                                                <td><?= $value->WANumber ?></td>
+                                                                <td><?= $value->alamat ?></td>
+                                                                <td>
+                                                                    <a class="btn btn-danger btn-action mr-1" data-id="<?= $value->ID ?>" onclick="confirmDelete(this)">
+                                                                        <i class="fas fa-trash-alt"></i>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                        <?php endforeach ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="card-body">
-                                  <div class="table-responsive">
-                                    <table class="table table-hover">
-                                        <thead> 
-                                            <tr>
-                                                <th scope="col">No</th>
-                                                <th scope="col">NamaLengkap</th>
-                                                <th scope="col">Nim</th>
-                                                <th scope="col">Wa Number</th>
-                                                <th scope="col">Alamat</th>
-                                                <th scope="col">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php foreach ($data as $key => $value) : ?>
-                                                <tr>
-                                                    <th scope="row"><?= ++$key ?></th>
-                                                    <td><?= $value->NamaLengkap ?></td>
-                                                    <td><?= $value->Nim ?></td>
-                                                    <td><?= $value->WANumber ?></td>
-                                                    <td><?= $value->alamat ?></td>
-                                                    <td>
-                                                        <!-- <a class="btn btn-primary btn-action mr-1" data-toggle="modal" data-target="#editModal" data-id="<?= $value->ID ?>"><i class="fas fa-pencil-alt"></i></a> -->
-                                                        <a class="btn btn-danger btn-action mr-1" data-id="<?= $value->ID ?>" onclick="confirmDelete(this)">
-                                                            <i class="fas fa-trash-alt"></i>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                            <?php endforeach ?>
-                                        </tbody>
-                                    </table>
+                                <div class="tab-pane fade" id="profile3" role="tabpanel" aria-labelledby="profile-tab3">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="table-responsive" style="max-height: 400px;">
+                                                <table class="table table-hover">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">No</th>
+                                                            <th scope="col">Mahasiswa</th>
+                                                            <th scope="col">Ayah</th>
+                                                            <th scope="col">Ibu</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php foreach ($dataOrtu as $key => $value) : ?>
+                                                            <tr>
+                                                                <th scope="row"><?= ++$key ?></th>
+                                                                <td><?= $value->NamaLengkap ?></td>
+                                                                <td><?= $value->nama_ayah ?></td>
+                                                                <td><?= $value->nama_ibu ?></td>
+                                                            </tr>
+                                                        <?php endforeach ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                </div>
-            </section>
+            </div>
+        </div>
+    </section>
+
 
 
     <!-- Modal Structure -->
@@ -134,6 +179,66 @@
                         </div>
                         <button type="submit" class="btn btn-primary">Upload CSV</button>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Structure -->
+    <div class="modal fade" id="multiFormModal" tabindex="-1" role="dialog" aria-labelledby="multiFormModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="multiFormModalLabel">Form Add</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Tab Navigation -->
+                    <ul class="nav nav-tabs" id="formTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="formMahasiswa-tab" data-toggle="tab" href="#formMahasiswa" role="tab" aria-controls="formMahasiswa" aria-selected="true">Mahasiswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="formOrangtua-tab" data-toggle="tab" href="#formOrangtua" role="tab" aria-controls="formOrangtua" aria-selected="false">Orangtua</a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab Content -->
+                    <div class="tab-content" id="formTabContent">
+                        <!-- Form Mahasiswa -->
+                        <div class="tab-pane fade show active" id="formMahasiswa" role="tabpanel" aria-labelledby="formMahasiswa-tab">
+                            <form id="formMahasiswaContent">
+                                <div class="form-group">
+                                    <label for="namaLengkap">Nama Lengkap</label>
+                                    <input type="text" class="form-control" id="namaLengkap" placeholder="Masukkan Nama Lengkap">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nim">NIM</label>
+                                    <input type="text" class="form-control" id="nim" placeholder="Masukkan NIM">
+                                </div>
+                            </form>
+                        </div>
+                        
+                        <!-- Form Orangtua -->
+                        <div class="tab-pane fade" id="formOrangtua" role="tabpanel" aria-labelledby="formOrangtua-tab">
+                            <form id="formOrangtuaContent">
+                                <div class="form-group">
+                                    <label for="namaAyah">Nama Ayah</label>
+                                    <input type="text" class="form-control" id="namaAyah" placeholder="Masukkan Nama Ayah">
+                                </div>
+                                <div class="form-group">
+                                    <label for="namaIbu">Nama Ibu</label>
+                                    <input type="text" class="form-control" id="namaIbu" placeholder="Masukkan Nama Ibu">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary" onclick="submitForms()">Simpan</button>
                 </div>
             </div>
         </div>
@@ -351,6 +456,17 @@
         }
     </script>
 
-</body>
+    <script>
+        $(document).ready(function () {
+            $('.table').DataTable({
+                "paging": true,        // Menampilkan pagination
+                "searching": true,     // Menampilkan search box
+                "ordering": true,      // Menampilkan sorting
+                "info": true          // Menampilkan informasi jumlah data
+            });
+        });
+    </script>
+
+    </body>
 
 </html>
