0d802129bd91375d29acafb454a20d7753db6ca6
Samriadi <basosamriadi01@gmail.com>
2024-11-05 16:01:25 +0700

    tes

--- app/Views/others/page_tagihan.php
+++ app/Views/others/page_tagihan.php
@@ -25,7 +25,7 @@
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
             <div class="breadcrumb-item"><a href="#">Invoice</a></div>
-            <div class="breadcrumb-item">Data Tagihann</div>
+            <div class="breadcrumb-item">Data Tagihan</div>
           </div>
         </div>
 
