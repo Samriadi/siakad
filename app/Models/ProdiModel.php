eafa3156542e504e0785ccdc19f336118203e2c1
Samriadi <basosamriadi01@gmail.com>
2024-11-18 15:59:23 +0800

    add prodi model

---
+++ app/Models/ProdiModel.php
@@ -0,0 +1,69 @@
+<?php
+
+class ProdiModel
+{
+  private $db;
+  private $mhs_prodi = 'mhs_prodi';
+
+
+  public function __construct()
+  {
+    global $mhs_prodi;
+    $this->db = Database::getInstance();
+  }
+
+  public function getAll()
+  {
+    $query = "SELECT   * FROM $this->mhs_prodi";
+    $stmt = $this->db->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
+  }
+
+  public function addData($data)
+  {
+    try {
+      $query = "INSERT INTO $this->mhs_paytype (nama_tagihan, jenis_tagihan) VALUES (?, ?)";
+      $stmt = $this->db->prepare($query);
+      $result = $stmt->execute([
+        $data['nama_tagihan'],
+        $data['jenis_tagihan']
+      ]);
+      return $result;
+    } catch (PDOException $e) {
+      error_log($e->getMessage());
+      return false;
+    }
+  }
+
+  public function updateData($data)
+  {
+    try {
+      $query = "UPDATE $this->mhs_paytype SET nama_tagihan = :nama_tagihan, jenis_tagihan = :jenis_tagihan WHERE recid = :id";
+      $stmt = $this->db->prepare($query);
+      $result = $stmt->execute([
+        ':nama_tagihan' => $data['nama_tagihan'],
+        ':jenis_tagihan' => $data['jenis_tagihan'],
+        ':id' => $data['id']
+      ]);
+
+      return $result;
+    } catch (PDOException $e) {
+      error_log($e->getMessage());
+      return false;
+    }
+  }
+
+  public function deleteData($id)
+  {
+    try {
+      $stmt = $this->db->prepare("DELETE FROM $this->mhs_paytype WHERE recid = :id");
+      $stmt->execute([
+        ':id' => $id
+      ]);
+      return $stmt->rowCount() > 0; 
+    } catch (PDOException $e) {
+      return false;
+    }
+  }
+}
