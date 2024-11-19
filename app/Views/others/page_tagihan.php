9a7fa05521bdc599cc0ca3c427ab73e08a4ca51f
Samriadi <basosamriadi01@gmail.com>
2024-11-19 13:11:37 +0800

    update page tagihan

--- app/Views/others/page_tagihan.php
+++ app/Views/others/page_tagihan.php
@@ -214,27 +214,25 @@
                 var dataProdi = data.dataProdi;
                 var dataAngkatan = data.dataAngkatan;
 
+                // Kosongkan dan tambahkan opsi placeholder untuk 'Prodi'
                 $('#add_prodi').empty().append('<option value="" selected disabled>Pilih Prodi</option>');
-
-                $.each(dataProdi, function(index, value) {
-                  $('#add_prodi').append('<option value="' + value.ID + '">' + value.deskripsi + '</option>');
+                dataProdi.forEach(item => {
+                    $('#add_prodi').append(`<option value="${item.ID}">${item.deskripsi}</option>`);
                 });
 
+                // Kosongkan dan tambahkan opsi placeholder untuk 'Jenis Tagihan'
                 $('#add_jenis_tagihan').empty().append('<option value="" selected disabled>Pilih Jenis Tagihan</option>');
-
-                $.each(dataPaytype, function(index, value) {
-                  $('#add_jenis_tagihan').append('<option value="' + value.recid + '">' + value.nama_tagihan + '</option>');
+                dataPaytype.forEach(item => {
+                    $('#add_jenis_tagihan').append(`<option value="${item.recid}">${item.nama_tagihan}</option>`);
                 });
 
-                $('#add_angkatan').empty().append('<option value="" selected disabled>Pilih Angkatan</option>');
-
-                // Tambahkan opsi "Semua Angkatan" di awal
-                $('#add_angkatan').append('<option value="Semua Angkatan">Semua Angkatan</option>');
-
-                $.each(dataAngkatan, function(index, value) {
-                  $('#add_angkatan').append('<option value="' + value.ID_angkatan + '">' + value.nama + '</option>');
+                // Kosongkan dan tambahkan opsi placeholder untuk 'Angkatan' dengan opsi tambahan "Semua Angkatan"
+                $('#add_angkatan').empty()
+                    .append('<option value="" selected disabled>Pilih Angkatan</option>')
+                    .append('<option value="Semua Angkatan">Semua Angkatan</option>');
+                dataAngkatan.forEach(item => {
+                    $('#add_angkatan').append(`<option value="${item.ID_angkatan}">${item.nama}</option>`);
                 });
-
 
                 $('#addModal').modal('show');
 
