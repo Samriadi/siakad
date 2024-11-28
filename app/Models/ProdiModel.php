f4d08665565d7cf94a2489f4c43068901099cb6a
Samriadi <basosamriadi01@gmail.com>
2024-11-28 10:50:33 +0800

    push prodi model

--- app/Models/ProdiModel.php
+++ app/Models/ProdiModel.php
@@ -12,41 +12,22 @@ class ProdiModel
     $this->db = Database::getInstance();
   }
 
-  public function getAll($where = [])
+  public function getAll()
   {
-      $query = "SELECT * FROM {$this->mhs_prodi}";
-
-      if (!empty($where)) {
-          $conditions = [];
-          foreach ($where as $column => $value) {
-              $conditions[] = "$column = :$column";
-          }
-          $query .= " WHERE " . implode(" AND ", $conditions);
-      }
-
-      $stmt = $this->db->prepare($query);
-
-      if (!empty($where)) {
-          foreach ($where as $column => $value) {
-              $stmt->bindValue(":$column", $value);
-          }
-      }
-
-      $stmt->execute();
-
-      return $stmt->fetchAll(PDO::FETCH_OBJ);
+    $query = "SELECT   * FROM $this->mhs_prodi";
+    $stmt = $this->db->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
   }
-
 
   public function addData($data)
   {
     try {
-      $query = "INSERT INTO $this->mhs_prodi (ID, name, deskripsi) VALUES (?, ?, ?)";
+      $query = "INSERT INTO $this->mhs_paytype (nama_tagihan, jenis_tagihan) VALUES (?, ?)";
       $stmt = $this->db->prepare($query);
       $result = $stmt->execute([
-        $data['ID'],
-        $data['name'],
-        $data['deskripsi']
+        $data['nama_tagihan'],
+        $data['jenis_tagihan']
       ]);
       return $result;
     } catch (PDOException $e) {
@@ -58,12 +39,12 @@ class ProdiModel
   public function updateData($data)
   {
     try {
-      $query = "UPDATE $this->mhs_prodi SET name = :name, deskripsi = :deskripsi WHERE ID = :ID";
+      $query = "UPDATE $this->mhs_paytype SET nama_tagihan = :nama_tagihan, jenis_tagihan = :jenis_tagihan WHERE recid = :id";
       $stmt = $this->db->prepare($query);
       $result = $stmt->execute([
-        ':name' => $data['name'],
-        ':deskripsi' => $data['deskripsi'],
-        ':ID' => $data['ID']
+        ':nama_tagihan' => $data['nama_tagihan'],
+        ':jenis_tagihan' => $data['jenis_tagihan'],
+        ':id' => $data['id']
       ]);
 
       return $result;
@@ -76,9 +57,9 @@ class ProdiModel
   public function deleteData($id)
   {
     try {
-      $stmt = $this->db->prepare("DELETE FROM $this->mhs_prodi WHERE ID = :ID");
+      $stmt = $this->db->prepare("DELETE FROM $this->mhs_paytype WHERE recid = :id");
       $stmt->execute([
-        ':ID' => $id
+        ':id' => $id
       ]);
       return $stmt->rowCount() > 0; 
     } catch (PDOException $e) {
