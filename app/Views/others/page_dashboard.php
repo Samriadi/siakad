36bb84213f398baa78f6ec2c24d32e2980411ae5
Samriadi <basosamriadi01@gmail.com>
2024-11-28 10:51:07 +0800

    modify page dashboard

--- app/Views/others/page_dashboard.php
+++ app/Views/others/page_dashboard.php
@@ -23,6 +23,10 @@
           <div class="section-header">
             <h1>SIAKAD - Dashboard</h1>
           </div>
+		  
+		  <br><center><h1>Versi 1.0</h1></center><br>
+		  
+		  <!--
           <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
               <div class="card card-statistic-1">
@@ -85,202 +89,51 @@
               </div>
             </div>                  
           </div>
+		  -->
+		  
        
+		  
           <div class="row">
           
             <div class="col-lg-6 col-md-6 col-12">
               <div class="card">
-                <div class="card-header">
-                  <h4 class="d-inline">Tasks</h4>
-                  <div class="card-header-action">
-                    <a href="#" class="btn btn-primary">View All</a>
-                  </div>
+ 				
+                <div class="card-body">
+				<h4>Jumlah Per-Prodi (All)</h4>
+				<ul>
+				<?php foreach ($prodi as $dt){
+                    echo "<li>".$dt->prodi." = ".$dt->jumlah."</li>";
+                }?>
+				</ul>
                 </div>
-                <div class="card-body">             
-                  <ul class="list-unstyled list-unstyled-border">
-                    <li class="media">
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="cbx-1">
-                        <label class="custom-control-label" for="cbx-1"></label>
-                      </div>
-                      <img class="mr-3 rounded-circle" width="50" src="assets/img/avatar/avatar-4.png" alt="avatar">
-                      <div class="media-body">
-                        <div class="badge badge-pill badge-danger mb-1 float-right">Not Finished</div>
-                        <h6 class="media-title"><a href="#">Redesign header</a></h6>
-                        <div class="text-small text-muted">Alfa Zulkarnain <div class="bullet"></div> <span class="text-primary">Now</span></div>
-                      </div>
-                    </li>
-                    <li class="media">
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="cbx-2" checked="">
-                        <label class="custom-control-label" for="cbx-2"></label>
-                      </div>
-                      <img class="mr-3 rounded-circle" width="50" src="assets/img/avatar/avatar-5.png" alt="avatar">
-                      <div class="media-body">
-                        <div class="badge badge-pill badge-primary mb-1 float-right">Completed</div>
-                        <h6 class="media-title"><a href="#">Add a new component</a></h6>
-                        <div class="text-small text-muted">Serj Tankian <div class="bullet"></div> 4 Min</div>
-                      </div>
-                    </li>
-                    <li class="media">
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="cbx-3" >
-                        <label class="custom-control-label" for="cbx-3"></label>
-                      </div>
-                      <img class="mr-3 rounded-circle" width="50" src="assets/img/avatar/avatar-2.png" alt="avatar">
-                      <div class="media-body">
-                        <div class="badge badge-pill badge-warning mb-1 float-right">Progress</div>
-                        <h6 class="media-title"><a href="#">Fix modal window</a></h6>
-                        <div class="text-small text-muted">Ujang Maman <div class="bullet"></div> 8 Min</div>
-                      </div>
-                    </li>
-                    <li class="media">
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="cbx-4">
-                        <label class="custom-control-label" for="cbx-4"></label>
-                      </div>
-                      <img class="mr-3 rounded-circle" width="50" src="assets/img/avatar/avatar-1.png" alt="avatar">
-                      <div class="media-body">
-                        <div class="badge badge-pill badge-danger mb-1 float-right">Not Finished</div>
-                        <h6 class="media-title"><a href="#">Remove unwanted classes</a></h6>
-                        <div class="text-small text-muted">Farhan A Mujib <div class="bullet"></div> 21 Min</div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
+				
+				
               </div>
             </div>
 
-          <div class="col-lg-6 col-md-8 col-12 col-sm-12">
-              <div class="card">
-                <div class="card-header">
-                  <h4>Latest Posts</h4>
-                  <div class="card-header-action">
-                    <a href="#" class="btn btn-primary">View All</a>
-                  </div>
-                </div>
+            <div class="col-lg-6 col-md-8 col-12 col-sm-12">
+              <div class="card">				
+				
                 <div class="card-body p-0">
-                  <div class="table-responsive">
-                    <table class="table table-striped mb-0">
-                      <thead>
-                        <tr>
-                          <th>Title</th>
-                          <th>Author</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>                         
-                        <tr>
-                          <td>
-                            Introduction Laravel 5
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Laravel 5 Tutorial - Installation
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Laravel 5 Tutorial - MVC
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Laravel 5 Tutorial - Migration
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Laravel 5 Tutorial - Deploy
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Laravel 5 Tutorial - Closing
-                            <div class="table-links">
-                              in <a href="#">Web Development</a>
-                              <div class="bullet"></div>
-                              <a href="#">View</a>
-                            </div>
-                          </td>
-                          <td>
-                            <a href="#" class="font-weight-600"><img src="assets/img/avatar/avatar-1.png" alt="avatar" width="30" class="rounded-circle mr-1"> Bagus Dwi Cahya</a>
-                          </td>
-                          <td>
-                            <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></a>
-                            <a class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete" data-confirm="Are You Sure?|This action can not be undone. Do you want to continue?" data-confirm-yes="alert('Deleted')"><i class="fas fa-trash"></i></a>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
+				<h4>Jumlah Per-Prodi (Aktif)</h4>
+				<ul>
+				<?php foreach ($mhsAktif as $dt){
+                    echo "<li>".$dt->prodi." = ".$dt->jumlah."</li>";
+                }?>
+				</ul>
                 </div>
+				
+				
+				
               </div>
             </div>
          
             
           </div>
+		  
+		  
+		  
+		  
         </section>
       </div>
 
