62534e5e74aba3ff2904085bd5ce7be1aab26011
Samriadi <basosamriadi01@gmail.com>
2024-11-29 11:10:09 +0800

    up page prodi

--- app/Views/others/page_prodi.php
+++ app/Views/others/page_prodi.php
@@ -41,23 +41,33 @@
                   </div>
                 </div>
                 <div class="card-body">
-                <div class="table-responsive" style="max-height: 770px; overflow-y: auto;">
+                  <div class="table-responsive" style="max-height: 770px; overflow-y: auto;">
                     <table class="table table-hover">
                       <thead style="position: sticky; top: 0; background-color: #fff; z-index: 1;">
                         <tr>
                           <th scope="col">No</th>
-                          <th scope="col">ID</th>
                           <th scope="col">Nama</th>
                           <th scope="col">Deskripsi</th>
+                          <th scope="col">Fakultas</th>
+                          <th scope="col">Action</th>
                         </tr>
                       </thead>
                       <tbody>
                         <?php foreach ($data as $key => $value) : ?>
                           <tr>
                             <th scope="row"><?= ++$key ?></th>
-                            <td><?= $value->ID ?></td>
                             <td><?= $value->name ?></td>
                             <td><?= $value->deskripsi ?></td>
+                            <td><?= $value->nama_fakultas ?></td>
+                            <td style="white-space: nowrap;">
+
+                              <a class="btn btn-primary btn-action mr-1 btn-edit" data-id="<?= $value->ID ?>">
+                                <i class="fas fa-pencil-alt"></i>
+                              </a>
+
+
+
+                            </td>
                           </tr>
                         <?php endforeach ?>
                       </tbody>
@@ -71,19 +81,145 @@
     </div>
     </section>
 
+
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="editModalLabel">Edit Data</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="card-body">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="name">Nama</label>
+                  <input type="text" class="form-control" id="name">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="deskripsi">Deskripsi</label>
+                  <input type="text" class="form-control" id="deskripsi">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="fakultas">Fakultas</label>
+                <select id="fakultas" class="form-control" name="fakultas" required>
+                  <option value="" selected disabled>Pilih Fakultas</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="submit">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
     <!-- Footer -->
     <?php include '../app/Views/others/layouts/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+
+
     <script>
-    $(document).ready(function() {
-        $('.table').DataTable({
-        "paging": true,
-        "searching": true,
-        "ordering": true,
-        "info": true
+      //edit data
+      $('.btn-edit').on('click', function() {
+
+        var id = $(this).data('id');
+
+        console.log(id)
+
+
+        $.ajax({
+          url: '/admin/siakad/prodi/fetch',
+          type: 'POST',
+          data: {
+            id: id,
+          },
+          dataType: 'json',
+          success: function(response) {
+            if (response.success) {
+              var data = response.data;
+              var dataFakultas = response.dataFakultas;
+              var optionFakultas = response.optionFakultas;
+
+
+              $('#fakultas').empty().append('<option value="' + dataFakultas.ID + '">' + dataFakultas.deskripsi + '</option>');
+
+              $.each(optionFakultas, function(index, item) {
+                if (!$('#fakultas option[value="' + item.ID + '"]').length) {
+                  $('#fakultas').append('<option value="' + item.ID + '">' + item.deskripsi + '</option>');
+                }
+              });
+
+
+
+              $('#name').val(data.name);
+              $('#deskripsi').val(data.deskripsi);
+
+
+              $('#editModal').modal('show');
+
+            } else {
+              console.log('Data tidak ditemukan');
+            }
+          }
         });
-    });
+
+        $('#submit').on('click', function() {
+
+          var arrayData = [{
+            ID: id,
+            name: $('#name').val(),
+            deskripsi: $('#deskripsi').val(),
+            fakultas: $('#fakultas').val()
+          }];
+
+          $.ajax({
+            url: '/admin/siakad/prodi/update',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(arrayData),
+            dataType: 'json',
+            success: function(response) {
+              if (response.success) {
+                Swal.fire({
+                  text: 'Your data has been updated.',
+                  icon: 'success',
+                  showConfirmButton: false,
+                  willClose: () => {
+                    window.location.reload();
+                  }
+                });
+              } else {
+                Swal.fire({
+                  text: 'An error occurred during updated.',
+                  icon: 'error',
+                  confirmButtonText: 'OK'
+                });
+
+              }
+            }
+          });
+
+        });
+
+      });
+
+
+      $(document).ready(function() {
+        $('.table').DataTable({
+          "paging": true,
+          "searching": true,
+          "ordering": true,
+          "info": true
+        });
+      });
     </script>
 
 
