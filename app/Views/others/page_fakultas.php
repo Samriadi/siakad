47e755c458b66c46290e5546b46f7458baf9b7fb
Samriadi <basosamriadi01@gmail.com>
2024-11-29 11:10:02 +0800

    add page fakultas

---
+++ app/Views/others/page_fakultas.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>SIAKAD - Fakultas</title>
+  <?php include '../app/Views/others/layouts/header.php'; ?>
+
+<body>
+  <div id="app">
+    <!-- <div class="main-wrapper main-wrapper-1"> -->
+
+    <!-- navbar -->
+    <?php include '../app/Views/others/layouts/topbar.php'; ?>
+
+    <!-- sidbar -->
+    <?php include '../app/Views/others/layouts/sidebar.php'; ?>
+
+    <!-- Main Content -->
+    <div class="main-content">
+      <section class="section">
+        <div class="section-header">
+          <h1>Data Fakultas</h1>
+          <div class="section-header-breadcrumb">
+            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+            <div class="breadcrumb-item"><a href="#">Invoice</a></div>
+            <div class="breadcrumb-item">Data Fakultas</div>
+          </div>
+        </div>
+
+        <div class="section-body">
+          <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+              <div class="card">
+                <div class="card-header">
+                  <div class="card-header-action">
+                    <a class="btn btn-primary" data-toggle="modal" data-target="#addModal">
+                      <i class="fas fa-plus text-white"></i>
+                    </a>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive" style="max-height: 770px; overflow-y: auto;">
+                    <table class="table table-hover">
+                      <thead style="position: sticky; top: 0; background-color: #fff; z-index: 1;">
+                        <tr>
+                          <th scope="col">No</th>
+                          <th scope="col">Nama</th>
+                          <th scope="col">Deskripsi</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php foreach ($data as $key => $value) : ?>
+                          <tr>
+                            <th scope="row"><?= ++$key ?></th>
+                            <td><?= $value->name ?></td>
+                            <td><?= $value->deskripsi ?></td>
+                          </tr>
+                        <?php endforeach ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    </section>
+
+    <!-- Footer -->
+    <?php include '../app/Views/others/layouts/footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+      $(document).ready(function() {
+        $('.table').DataTable({
+          "paging": true,
+          "searching": true,
+          "ordering": true,
+          "info": true
+        });
+      });
+    </script>
+
+
+</body>
+
+</html>
