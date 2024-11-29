437a4d22d2b3ed3fe57e4f1121cd37e00a1ffc33
Samriadi <basosamriadi01@gmail.com>
2024-11-29 11:10:17 +0800

    up page tagihan

--- app/Views/others/page_tagihan.php
+++ app/Views/others/page_tagihan.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>SIAKAD - Tagihan</title>
+  <title>SIAKAD - Master Tagihan</title>
   <?php include '../app/Views/others/layouts/header.php'; ?>
 
 <body>
@@ -25,7 +25,7 @@
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
             <div class="breadcrumb-item"><a href="#">Invoice</a></div>
-            <div class="breadcrumb-item">Data Tagihann</div>
+            <div class="breadcrumb-item">Master Tagihan</div>
           </div>
         </div>
 
@@ -34,7 +34,7 @@
             <div class="col-12 col-md-12 col-lg-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>Data Tagihan</h4>
+                  <h4>Master Tagihan</h4>
                   <div class="card-header-action">
                     <a class="btn btn-primary" id="btn-add">
                       <i class="fas fa-plus text-white"></i>
@@ -47,6 +47,7 @@
                       <thead>
                         <tr>
                           <th scope="col">No</th>
+                          <th scope="col">Fakultas</th>
                           <th scope="col">Program Studi</th>
                           <th scope="col">Jenis Tagihan</th>
                           <th scope="col">Angkatan</th>
@@ -59,6 +60,7 @@
                         <?php foreach ($data as $key => $value) : ?>
                           <tr>
                             <th scope="row"><?= ++$key ?></th>
+                            <td><?= $value->nama_fakultas ?></td>
                             <td><?= $value->nama_prodi ?></td>
                             <td><?= $value->nama_tagihan ?></td>
                             <td><?= $value->nama_angkatan ?></td>
@@ -70,7 +72,7 @@
                                 <i class="fas fa-pencil-alt"></i>
                               </a>
 
-                              <a class="btn btn-danger btn-action mr-1" data-id="<?= $value->recid ?>"  onclick="confirmDelete(this)">
+                              <a class="btn btn-danger btn-action mr-1" data-id="<?= $value->recid ?>" onclick="confirmDelete(this)">
                                 <i class="fas fa-trash-alt"></i>
                               </a>
 
@@ -91,57 +93,63 @@
 
     <!-- Modal Structure -->
     <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="addModalLabel">Add Data</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addModalLabel">Add Data</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form id="addForm">
+            <div class="modal-body">
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="add_fakultas">Fakultas</label>
+                  <select id="add_fakultas" class="form-control" name="fakultas" required>
+                    <option value="" selected disabled>Pilih Fakultas</option>
+                  </select>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="add_prodi">Program Studi</label>
+                    <select id="add_prodi" class="form-control" name="prodi" required>
+                      <option value="" selected disabled>Pilih Prodi</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="add_angkatan">Angkatan</label>
+                    <select id="add_angkatan" class="form-control" name="angkatan" required>
+                      <option value="" selected disabled>Pilih Angkatan</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="add_jenis_tagihan">Jenis Tagihan</label>
+                    <select id="add_jenis_tagihan" class="form-control" name="jenis_tagihan" required>
+                      <option value="" selected disabled>Pilih Jenis Tagihan</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="add_nominal">Nominal</label>
+                    <input type="number" class="form-control" id="add_nominal" name="nominal">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="add_keterangan">Keterangan</label>
+                  <input type="text" class="form-control" id="add_keterangan" name="keterangan">
+                </div>
+              </div>
             </div>
-            <form id="addForm">
-                <div class="modal-body">
-                <div class="card-body">
-                    <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="add_prodi">Program Studi</label>
-                        <select id="add_prodi" class="form-control" name="prodi" required>
-                        <option value="" selected disabled>Pilih Prodi</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-6">						
-                        <label for="add_angkatan">Angkatan</label>
-                        <select id="add_angkatan" class="form-control" name="angkatan" required>
-                        <option value="" selected disabled>Pilih Angkatan</option>
-                        </select>						
-                    </div>
-                    </div>
-                    <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="add_jenis_tagihan">Jenis Tagihan</label>
-                        <select id="add_jenis_tagihan" class="form-control" name="jenis_tagihan" required>
-                        <option value="" selected disabled>Pilih Jenis Tagihan</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="add_nominal">Nominal</label>
-                        <input type="number" class="form-control" id="add_nominal" name="nominal">
-                    </div>
-                    </div>
-                    <div class="form-group">
-                    <label for="add_keterangan">Keterangan</label>
-                    <input type="text" class="form-control" id="add_keterangan" name="keterangan">
-                    </div>
-                </div>
-                </div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary" id="add_submit">Submit</button>
-                </div>
-            </form>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary" id="add_submit">Submit</button>
             </div>
+          </form>
         </div>
-        </div>
+      </div>
+    </div>
 
 
     <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
@@ -162,17 +170,17 @@
                     <option value="" selected disabled>Pilih Prodi</option>
                   </select>
                 </div>
-                <div class="form-group col-md-6">				  
+                <div class="form-group col-md-6">
                   <label for="angkatan">Angkatan</label>
                   <select id="angkatan" class="form-control" name="angkatan" required>
                     <option value="" selected disabled>Pilih Angkatan</option>
-                  </select>				  
+                  </select>
                 </div>
               </div>
               <div class="form-row">
                 <div class="form-group col-md-6">
                   <label for="jenis_tagihan">Jenis Tagihan</label>
-                    <select id="jenis_tagihan" class="form-control" name="jenis_tagihan" required>
+                  <select id="jenis_tagihan" class="form-control" name="jenis_tagihan" required>
                     <option value="" selected disabled>Pilih Jenis Tagihan</option>
                   </select>
                 </div>
@@ -181,10 +189,10 @@
                   <input type="number" class="form-control" id="nominal">
                 </div>
               </div>
-                <div class="form-group">
-                  <label for="keterangan">Keterangan</label>
-                  <input type="text" class="form-control" id="keterangan">
-                </div>
+              <div class="form-group">
+                <label for="keterangan">Keterangan</label>
+                <input type="text" class="form-control" id="keterangan">
+              </div>
             </div>
           </div>
           <div class="modal-footer">
@@ -213,12 +221,32 @@
                 var dataPaytype = data.dataPaytype;
                 var dataProdi = data.dataProdi;
                 var dataAngkatan = data.dataAngkatan;
+                var dataFakultas = data.dataFakultas;
 
-                $('#add_prodi').empty().append('<option value="" selected disabled>Pilih Prodi</option>');
 
-                $.each(dataProdi, function(index, value) {
-                  $('#add_prodi').append('<option value="' + value.ID + '">' + value.deskripsi + '</option>');
+
+                $.each(dataFakultas, function(index, fakultas) {
+                  $('#add_fakultas').append('<option value="' + fakultas.ID + '">' + fakultas.deskripsi + '</option>');
                 });
+
+                $('#add_fakultas').on('change', function() {
+                  const selectedFakultasID = $(this).val();
+                  $('#add_prodi').empty().append('<option value="" selected disabled>Pilih Prodi</option>');
+
+                  const defaultProdi = dataProdi.filter(prodi => prodi.fakultas == 0);
+
+                  $.each(defaultProdi, function(index, prodi) {
+                    $('#add_prodi').append('<option value="' + prodi.ID + '">' + prodi.deskripsi + '</option>');
+                  });
+
+                  const filteredProdi = dataProdi.filter(prodi => prodi.fakultas == selectedFakultasID);
+
+                  $.each(filteredProdi, function(index, prodi) {
+                    $('#add_prodi').append('<option value="' + prodi.ID + '">' + prodi.deskripsi + '</option>');
+                  });
+                });
+
+
 
                 $('#add_jenis_tagihan').empty().append('<option value="" selected disabled>Pilih Jenis Tagihan</option>');
 
@@ -250,51 +278,52 @@
         });
 
         $('#add_submit').on('click', async function(event) {
-          event.preventDefault(); 
-          
+          event.preventDefault();
+
           // Ambil data dari input dan buat array
           var arrayData = [{
-              prodi: $('#add_prodi').val(),
-              jenis_tagihan: $('#add_jenis_tagihan').val(),
-              angkatan: $('#add_angkatan').val(),
-              nominal: $('#add_nominal').val(),
-              keterangan: $('#add_keterangan').val(),
+            fakultas: $('#add_fakultas').val(),
+            prodi: $('#add_prodi').val(),
+            jenis_tagihan: $('#add_jenis_tagihan').val(),
+            angkatan: $('#add_angkatan').val(),
+            nominal: $('#add_nominal').val(),
+            keterangan: $('#add_keterangan').val(),
           }];
 
           try {
-              let response = await $.ajax({
-                  url: '/admin/siakad/tagihan/add',
-                  type: 'POST',
-                  contentType: 'application/json',
-                  data: JSON.stringify(arrayData),
-                  dataType: 'json',
-              });
+            let response = await $.ajax({
+              url: '/admin/siakad/tagihan/add',
+              type: 'POST',
+              contentType: 'application/json',
+              data: JSON.stringify(arrayData),
+              dataType: 'json',
+            });
 
-              if (response.success) {
-                  await Swal.fire(
-                      'Added!',
-                      response.message,  
-                      'success'
-                  ).then(() => {
-                      location.reload(); 
-                  });
-              } else {
-                  await Swal.fire({
-                      text: response.message, 
-                      icon: 'warning',
-                      confirmButtonText: 'OK'
-                  });
-              }
+            if (response.success) {
+              await Swal.fire(
+                'Added!',
+                response.message,
+                'success'
+              ).then(() => {
+                location.reload();
+              });
+            } else {
+              await Swal.fire({
+                text: response.message,
+                icon: 'warning',
+                confirmButtonText: 'OK'
+              });
+            }
 
           } catch (error) {
-              console.error('AJAX Error:', error);
-              await Swal.fire({
-                  text: 'An unexpected error occurred.',
-                  icon: 'error',
-                  confirmButtonText: 'OK'
-              });
+            console.error('AJAX Error:', error);
+            await Swal.fire({
+              text: 'An unexpected error occurred.',
+              icon: 'error',
+              confirmButtonText: 'OK'
+            });
           }
-      });
+        });
 
 
 
@@ -303,7 +332,7 @@
         $('.btn-edit').on('click', function() {
 
           var data = <?php echo json_encode($data) ?>;
-          
+
           var id = $(this).data('id');
 
 
@@ -346,18 +375,18 @@
 
                 // Jika ada dataAngkatan, tambahkan opsi yang dipilih terlebih dahulu
                 if (dataAngkatan) {
-                    $('#angkatan').append('<option value="' + dataAngkatan.ID_angkatan + '" selected>' + dataAngkatan.nama + '</option>');
-                    $('#angkatan').append('<option value="Semua Angkatan">Semua Angkatan</option>');
+                  $('#angkatan').append('<option value="' + dataAngkatan.ID_angkatan + '" selected>' + dataAngkatan.nama + '</option>');
+                  $('#angkatan').append('<option value="Semua Angkatan">Semua Angkatan</option>');
                 } else {
-                    // Jika tidak ada dataAngkatan, hanya tambahkan opsi "Semua Angkatan" sebagai default
-                    $('#angkatan').append('<option value="Semua Angkatan">Semua Angkatan</option>');
+                  // Jika tidak ada dataAngkatan, hanya tambahkan opsi "Semua Angkatan" sebagai default
+                  $('#angkatan').append('<option value="Semua Angkatan">Semua Angkatan</option>');
                 }
 
                 // Tambahkan opsi lainnya dari optionAngkatan, hindari duplikat
                 $.each(optionAngkatan, function(index, item) {
-                    if (!$('#angkatan option[value="' + item.ID_angkatan + '"]').length) {
-                        $('#angkatan').append('<option value="' + item.ID_angkatan + '">' + item.nama + '</option>');
-                    }
+                  if (!$('#angkatan option[value="' + item.ID_angkatan + '"]').length) {
+                    $('#angkatan').append('<option value="' + item.ID_angkatan + '">' + item.nama + '</option>');
+                  }
                 });
 
 
@@ -467,15 +496,15 @@
       }
     </script>
 
-<script>
-    $(document).ready(function() {
+    <script>
+      $(document).ready(function() {
         $('.table').DataTable({
-        "paging": true,
-        "searching": true,
-        "ordering": true,
-        "info": true
+          "paging": true,
+          "searching": true,
+          "ordering": true,
+          "info": true
         });
-    });
+      });
     </script>
 
 </body>
