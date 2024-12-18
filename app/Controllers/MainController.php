5a88da64324d91a7934fc4f23d20c4b8dca1660f
Samriadi <basosamriadi01@gmail.com>
2024-12-18 09:34:50 +0800

    update main controller

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,39 +1,42 @@
 <?php
 
-class MainController{
-	
-	private $MahasiswaModel;
+class MainController
+{
+
+    private $MahasiswaModel;
 
     public function __construct()
     {
-      $this->checkLogin();
-	  $this->MahasiswaModel = new MahasiswaModel();
+        $this->checkLogin();
+        $this->MahasiswaModel = new MahasiswaModel();
     }
 
-    public function checkLogin() {
+    public function checkLogin()
+    {
         if (!isset($_SESSION['user_loged'])) {
             header("Location: /admin/login");
             exit();
         }
-      }
-      
-    public function index(){
-        
+    }
+
+    public function index()
+    {
+
         if (!isset($_SESSION['user_loged'])) {
             header("Location: /admin/login");
             exit();
         }
-		
+
         $prodi = $this->MahasiswaModel->countBy("deskripsi", "prodi");
-		$mhsAktif = $this->MahasiswaModel->countBy("deskripsi", "prodi", "Aktif");
-    
+        $mhsAktif = $this->MahasiswaModel->countBy("deskripsi", "prodi", "Aktif");
+        $userAccess = $this->MahasiswaModel->getAccess($_SESSION['user_loged']);
+
         // Jika sesi ada, tampilkan halaman dashboard
         include __DIR__ . '/../Views/others/page_dashboard.php';
     }
-    
+
     public function selectDash()
     {
         include __DIR__ . '/../Views/others/page_selectDash.php';
     }
-
 }
