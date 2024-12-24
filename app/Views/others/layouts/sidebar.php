3fb08466c4284b6cdf81d39dc962e448b7f90713
Samriadi <basosamriadi01@gmail.com>
2024-12-24 11:15:52 +0800

    update sidebar

--- app/Views/others/layouts/sidebar.php
+++ app/Views/others/layouts/sidebar.php
@@ -28,9 +28,9 @@
           <li class="nav-item"><a class="nav-link" href="/admin/siakad/pembayaran">Payment Type</a></li>
           <li class="nav-item"><a class="nav-link" href="/admin/siakad/tagihan">Master Tagihan</a></li>
           <li class="nav-item"><a class="nav-link" href="/admin/siakad/transaksi">Transaksi Tagihan</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/multi-transaksi">Multi Tagihan</a></li>
+          <!-- <li class="nav-item"><a class="nav-link" href="/admin/siakad/multi-transaksi">Multi Tagihan</a></li> -->
           <li class="nav-item"><a class="nav-link" href="/admin/siakad/invoice">Tagihan Mahasiswa</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/pelunasan">Pelunasan</a></li>
+          <li class="nav-item"><a class="nav-link" href="/admin/siakad/pelunasan">Pembayaran Tunai</a></li>
 
         </ul>
       </li>
