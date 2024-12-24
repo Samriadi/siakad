0ba4c0a33d15243eb7aa4cc20a81c047a86b2ab4
Samriadi <basosamriadi01@gmail.com>
2024-12-24 11:15:02 +0800

    update page pelunasan

--- app/Views/others/page_pelunasan.php
+++ app/Views/others/page_pelunasan.php
@@ -33,10 +33,10 @@
     <div class="main-content">
       <section class="section">
         <div class="section-header">
-          <h1>Pelunasan Tagihan</h1>
+          <h1>Pembayaran Tunai</h1>
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Pelunasan</a></div>
+            <div class="breadcrumb-item"><a href="#">Pembayaran Tunai</a></div>
           </div>
         </div>
 
