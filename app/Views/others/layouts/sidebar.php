8fa3620186c733f4c6658a640dc9f26db7cae57d
Samriadi <basosamriadi01@gmail.com>
2024-12-30 04:47:27 +0800

    update sidebar

--- app/Views/others/layouts/sidebar.php
+++ app/Views/others/layouts/sidebar.php
@@ -6,19 +6,23 @@
     <div class="sidebar-brand sidebar-brand-sm">
     </div>
 
-    <ul class="sidebar-menu">
-      <li class="menu-header">Member Area</li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-        <ul class="dropdown-menu">
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/">General Information</a></li>
-          <?php if ($_SESSION['user_loged'] == 'kaprodi' || $_SESSION['user_loged'] == 'superadmin') : ?>
-            <li class="nav-item"><a class="nav-link" href="/admin/siakad/setting">Setup</a></li>
-          <?php endif ?>
+    <?php if ($_SESSION['level_loged'] == 'superadmin') : ?>
 
-        </ul>
-      </li>
-    </ul>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Member Area</li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+          <ul class="dropdown-menu">
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/">General Information</a></li>
+            <?php if ($_SESSION['user_loged'] == 'kaprodi' || $_SESSION['user_loged'] == 'superadmin') : ?>
+              <li class="nav-item"><a class="nav-link" href="/admin/siakad/setting">Setup</a></li>
+            <?php endif ?>
+
+          </ul>
+        </li>
+      </ul>
+
+    <?php endif ?>
 
     <ul class="sidebar-menu">
       <li class="menu-header">TRANSAKSI</li>
@@ -68,27 +72,30 @@
 
     </ul>
 
+    <?php if ($_SESSION['level_loged'] == 'superadmin') : ?>
 
-    <ul class="sidebar-menu">
+      <ul class="sidebar-menu">
 
-      <li class="menu-header">Setup</li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Master Data</span></a>
-        <ul class="dropdown-menu">
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/matkul">Mata Kuliah</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/mahasiswa">Mahasiswa</a></li>
-          <!-- <li class="nav-item"><a class="nav-link" href="/admin/siakad/ortu">Orang Tua</a></li> -->
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/dosen">Dosen</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/prodi">Program Studi</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/fakultas">Fakultas</a></li>
-          <li class="nav-item"><a class="nav-link" href="/admin/siakad/angkatan">Angkatan</a></li>
-          <?php if ($_SESSION['user_loged'] == 'kaprodi' || $_SESSION['user_loged'] == 'superadmin') : ?>
-            <!-- <li class="nav-item"><a class="nav-link" href="/admin/siakad/staff">Staff</a></li> -->
-          <?php endif ?>
-        </ul>
-      </li>
+        <li class="menu-header">Setup</li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Master Data</span></a>
+          <ul class="dropdown-menu">
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/matkul">Mata Kuliah</a></li>
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/mahasiswa">Mahasiswa</a></li>
+            <!-- <li class="nav-item"><a class="nav-link" href="/admin/siakad/ortu">Orang Tua</a></li> -->
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/dosen">Dosen</a></li>
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/prodi">Program Studi</a></li>
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/fakultas">Fakultas</a></li>
+            <li class="nav-item"><a class="nav-link" href="/admin/siakad/angkatan">Angkatan</a></li>
+            <?php if ($_SESSION['user_loged'] == 'kaprodi' || $_SESSION['user_loged'] == 'superadmin') : ?>
+              <!-- <li class="nav-item"><a class="nav-link" href="/admin/siakad/staff">Staff</a></li> -->
+            <?php endif ?>
+          </ul>
+        </li>
 
-    </ul>
+      </ul>
+
+    <?php endif ?>
 
   </aside>
 </div>
