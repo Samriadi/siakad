7025eed434197ad2d5c45a33344f33af3f4b392b
Samriadi <basosamriadi01@gmail.com>
2025-01-01 08:31:34 +0800

    create add modal page ruangan

--- app/Views/others/page_ruangan.php
+++ app/Views/others/page_ruangan.php
@@ -70,12 +70,94 @@
     </div>
     </section>
 
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addModalLabel">Add Master Ruangan</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="card-body">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="add_name">Name</label>
+                  <input type="text" class="form-control" id="add_name">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="add_capacity">Capacity</label>
+                  <input type="number" class="form-control" id="add_capacity">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="add_desc">Description</label>
+                <input type="text" class="form-control" id="add_desc">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="add_submit">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
     <!-- Footer -->
     <?php include '../app/Views/others/layouts/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
       $(document).ready(function() {
+
+        //add data
+        $('#add_submit').on('click', function() {
+          var arrayData = [{
+            name: $('#add_name').val(),
+            capacity: $('#add_capacity').val(),
+            description: $('#add_desc').val()
+          }];
+
+          $.ajax({
+            url: '/admin/siakad/ruangan/add',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(arrayData),
+            dataType: 'json',
+            success: function(response) {
+              console.log(response);
+              if (response.success) {
+                Swal.fire({
+                  text: 'Your data has been added.',
+                  icon: 'success',
+                  showConfirmButton: false,
+                  willClose: () => {
+                    window.location.reload();
+                  }
+                });
+              } else {
+                Swal.fire({
+                  text: 'An error occurred during the add.',
+                  icon: 'error',
+                  confirmButtonText: 'OK'
+                });
+              }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+              console.error('AJAX Error:', textStatus, errorThrown);
+              Swal.fire({
+                text: 'An unexpected error occurred.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+              });
+            }
+          });
+        });
+
+        //paging
         $('.table').DataTable({
           "paging": true,
           "searching": true,
@@ -84,6 +166,8 @@
         });
       });
     </script>
+
+
 
 
 </body>
