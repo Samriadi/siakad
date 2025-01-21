6a75c1bb0e524e9b73112c10a3c03c9e4b8cc29a
Samriadi <basosamriadi01@gmail.com>
2025-01-22 00:57:48 +0800

    create profile controlller

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,52 @@
+<?php
+
+class ProfileController
+{
+
+  private $ProfileModel;
+  private $dataProfile;
+
+  public function __construct()
+  {
+    $this->checkLogin();
+
+    $this->ProfileModel = new ProfileModel();
+    $this->dataProfile = $this->ProfileModel->getProfile(['username' => $_SESSION['user_loged']]);
+  }
+  public function checkLogin()
+  {
+    if (!isset($_SESSION['user_loged'])) {
+      header("Location: /admin/login");
+      exit();
+    }
+  }
+  public function index()
+  {
+
+    $data = $this->dataProfile;
+
+    include __DIR__ . '/../Views/others/page_profile.php';
+  }
+
+  public function changesData()
+  {
+    $dataArray = json_decode(file_get_contents('php://input'), true);
+
+    if ($dataArray === null) {
+      $response = [
+        'success' => false,
+        'message' => 'Invalid JSON input'
+      ];
+    } else {
+      $request = $this->ProfileModel->changesData($dataArray[0]);
+
+      $response = [
+        'success' => $request,
+        'message' => $request ? 'Data berhasil diupdate' : 'Update failed',
+      ];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+  }
+}
