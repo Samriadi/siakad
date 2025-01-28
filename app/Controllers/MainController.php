b6666fbcc7a400e022063b9be56bbc53d4396347
Samriadi <basosamriadi01@gmail.com>
2025-01-28 11:59:35 +0800

    update main controller

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,49 +1,51 @@
 <?php
-class MainController{
-	
-	private $MahasiswaModel;
+class MainController
+{
+
+    private $MahasiswaModel;
 
     public function __construct()
     {
-      $this->checkLogin();
-	  $this->MahasiswaModel = new MahasiswaModel();
+        $this->checkLogin();
+        $this->MahasiswaModel = new MahasiswaModel();
     }
 
-    public function checkLogin() {
+    public function checkLogin()
+    {
         if (!isset($_SESSION['user_loged'])) {
             header("Location: /admin/login");
             exit();
         }
-      }
-      
-    public function index(){
-        
+    }
+
+    public function index()
+    {
+
         if (!isset($_SESSION['user_loged'])) {
             header("Location: /admin/login");
             exit();
         }
-		
+
         $prodi = $this->MahasiswaModel->countBy("deskripsi", "prodi");
-		$mhsAktif = $this->MahasiswaModel->countBy("deskripsi", "prodi", "Aktif");
-		$userAccess = $this->MahasiswaModel->getAccess($_SESSION['user_loged']);
-    
+        $mhsAktif = $this->MahasiswaModel->countBy("deskripsi", "prodi", "Aktif");
+        $userAccess = $this->MahasiswaModel->getAccess($_SESSION['user_loged']);
+
         // Jika sesi ada, tampilkan halaman dashboard
         include __DIR__ . '/../Views/others/page_dashboard.php';
     }
-    
+
     public function selectDash()
     {
-          if (isset($_SESSION['user_modul'])) {
-              if ($_SESSION['user_modul'] == "pmb") {
-                  header("Location: /");
-                  exit();
-              } elseif ($_SESSION['user_modul'] == "mhs") {
-                  header("Location: /siakad");
-                  exit();
-              }
-          }
-        
+        if (isset($_SESSION['user_modul'])) {
+            if ($_SESSION['user_modul'] == "pmb") {
+                header("Location: /");
+                exit();
+            } elseif ($_SESSION['user_modul'] == "mhs") {
+                header("Location: /siakad/portal-mhs");
+                exit();
+            }
+        }
+
         include __DIR__ . '/../Views/others/page_selectDash.php';
     }
-
 }
