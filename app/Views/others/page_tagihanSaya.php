8b9650e67ff3b55a2d2e2f8dfb1694ef7ca094ca
Samriadi <basosamriadi01@gmail.com>
2025-02-03 09:41:48 +0800

    up

--- app/Views/others/page_tagihanSaya.php
+++ app/Views/others/page_tagihanSaya.php
@@ -33,7 +33,7 @@
               <div class="col-12 col-md-12 col-lg-12">
                 <div class="card">
                   <div class="card-body">
-                    <div class="form-row">
+                    <div class="row">
                       <div class="form-group col-md-6">
                         <label>NIM</label>
                         <input type="number" class="form-control" id="nim" name="nim" disabled>
@@ -43,33 +43,50 @@
                         <input type="text" class="form-control" id="nama" name="nama" disabled>
                       </div>
                     </div>
-                    <div class="table-responsive">
-                      <table class="table table-bordered table-md">
-                        <tr>
-                          <th scope="col">Virtual Account</th>
-                          <th scope="col">Periode</th>
-                          <th scope="col">Tagihan Baru</th>
-                          <th scope="col">Total Pembayaran</th>
-                          <th scope="col">Status</th>
-                        </tr>
-                        <?php foreach ($data as $key => $value) :
-                          if ($value->total_pembayaran >= $value->tagihan)
-                            $status = "Lunas";
-                          else
-                            $status = "Belum Lunas";
-                        ?>
-                          <tr>
-                            <td><?= $value->va_number ?></td>
-                            <td><?= $value->periode ?></td>
-                            <td>Rp. <?= number_format($value->tagihan, 0) ?></td>
-                            <td>Rp. <?= number_format($value->total_pembayaran, 0) ?></td>
-                            <td>Rp. <?= $status ?></td>
-                          </tr>
-                        <?php endforeach ?>
-                      </table>
+                    <div class="row">
+                      <div class="form-group col-md-6">
+                        <label>Fakultas</label>
+                        <input type="text" class="form-control" id="fakultas" name="fakultas" disabled>
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label>Program Studi</label>
+                        <input type="text" class="form-control" id="prodi" name="prodi" disabled>
+                      </div>
                     </div>
+
+
+                    <?php foreach ($data as $key => $value) :
+                      $status = ($value->total_pembayaran >= $value->tagihan) ? "Lunas" : "Belum Lunas";
+                    ?>
+                      <div class="row border p-3 mb-3">
+                        <div class="col-12 mb-2">
+                          <label>Virtual Account</label>
+                          <input type="text" class="form-control" name="va_number[<?= $key ?>]" value="<?= $value->va_number ?>" readonly>
+                        </div>
+                        <div class="col-12 mb-2">
+                          <label>Periode</label>
+                          <input type="text" class="form-control" name="periode[<?= $key ?>]" value="<?= $value->periode ?>" readonly>
+                        </div>
+                        <div class="col-12 mb-2">
+                          <label>Tagihan Baru</label>
+                          <input type="text" class="form-control" name="tagihan[<?= $key ?>]" value="Rp. <?= number_format($value->tagihan, 0) ?>" readonly>
+                        </div>
+                        <div class="col-12 mb-2">
+                          <label>Total Pembayaran</label>
+                          <input type="text" class="form-control" name="total_pembayaran[<?= $key ?>]" value="Rp. <?= number_format($value->total_pembayaran, 0) ?>" readonly>
+                        </div>
+                        <div class="col-12 mb-2">
+                          <label>Status</label>
+                          <input type="text" class="form-control font-weight-bold" name="status[<?= $key ?>]" value="<?= $status ?>" readonly>
+                        </div>
+                      </div>
+                    <?php endforeach; ?>
+
+
                   </div>
                 </div>
+
+
               </div>
             </div>
           </div>
@@ -86,6 +103,8 @@
   $(document).ready(function() {
     $('#nim').val(data[0].nim);
     $('#nama').val(data[0].nama);
+    $('#fakultas').val(data[0].nama_fakultas);
+    $('#prodi').val(data[0].nama_prodi);
   });
 </script>
 
